b5111e98805067525362340b68c158815a830e53
Roman Lenart <slicercro@gmail.com>
2015-06-24 00:44:20 +0200

    Roman
    Izrađen backend za unos CRUD/Index. Ne radi, ne javlja grešku, ali ni ne
    unese u bazu.

--- Where2Study/Controllers/CRUDController.cs
+++ Where2Study/Controllers/CRUDController.cs
@@ -145,35 +145,180 @@ namespace Where2Study.Controllers
         }
 
         [AcceptVerbs(HttpVerbs.Post), ValidateInput(false)]
-        public ActionResult Create(drzava country, drzava_tekst country_text, grad city, grad_tekst city_text, fakultet faculty, fakultet_tekst faculty_text, sveuciliste university, sveuciliste_tekst university_text)
+        //public ActionResult Create(kontinent_tekst continent, drzava_tekst country_text, grad_tekst city_text, fakultet faculty, fakultet_tekst faculty_text, sveuciliste university, sveuciliste_tekst university_text)
+        public ActionResult Create(Faculty facultyEntry)
  
         {
             if (ModelState.IsValid)
             {
-                NameValueCollection nvc = Request.Form;
-                try
-                {
-                    UpdateModel(country);
-                    repository.Add(country);
-                    repository.Save();
-                    return RedirectToAction("Details", new { id = country.id });
+                //NameValueCollection nvc = Request.Form;
+                var db = new w2sDataContext(); 
+                var currentLanguage = Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName;
+                var clId=0;
+                kontinent_tekst continent = new kontinent_tekst();
+                continent.tekst = facultyEntry.Continent;
 
-                    UpdateModel(city);
-                    repository.Add(city);
-                    repository.Save();
-                    return RedirectToAction("Details", new { id = city.id });
+                drzava country = new drzava();
+                drzava_tekst country_text = new drzava_tekst();
+                country_text.naziv = facultyEntry.Country;
+                country_text.opis = "";
 
-                    UpdateModel(faculty);
-                    repository.Add(faculty);
-                    repository.Save();
-                    return RedirectToAction("Details", new { id = faculty.id });
-                }
-                catch
+                grad city = new grad();
+                grad_tekst city_text = new grad_tekst();
+                city_text.naziv = facultyEntry.City;
+                city_text.opis = "";
+
+                sveuciliste university = new sveuciliste();
+                university.adresa_sveucilista = ""; 
+                university.broj_telefona = null;
+                university.web = "";                           
+
+                sveuciliste_tekst university_text = new sveuciliste_tekst();
+                university_text.naziv = facultyEntry.University;
+                university_text.opis = "";
+
+                fakultet faculty = new fakultet();
+                faculty.adresa_fakulteta = facultyEntry.Address;
+                faculty.broj_telefona = facultyEntry.Phone;
+                faculty.web = facultyEntry.WebSite;
+                faculty.slika = facultyEntry.Photo;
+                
+                fakultet_tekst faculty_text = new fakultet_tekst();
+                faculty_text.naziv = facultyEntry.Title;
+                faculty_text.opis = facultyEntry.Description;
+
+
+                //sveuciliste university = new sveuciliste();
+                //fakultet faculty = new fakultet();
+
+                var languages = from j in db.jeziks select j;
+                foreach(var item in languages) if(currentLanguage == item.kratica) clId=item.id;    
+                bool countryBool=false, cityBool=false, universityBool=false, facultyBool=false;
+                var countries = from d in db.drzavas
+                            from dt in db.drzava_teksts
+                            where d.id == dt.id_drzava
+                            select dt;
+                foreach(var item in countries)
                 {
-                     ModelState.AddRuleViolations(faculty.GetRuleViolations());
+                    if (country_text.naziv == item.naziv) countryBool=true;
                 }
-            }
-            return View(faculty);
+                if (countryBool==false)
+                    try
+                    {
+                        var continents = from kt in db.kontinent_teksts
+                                         from k in db.kontinents
+                                         where k.id==kt.id_kontinent && kt.id_jezik==clId
+                                         select kt;
+                        foreach(var item in continents) if(continent.tekst == item.tekst) country.id_kontinent=item.id_kontinent;
+                        UpdateModel(country);
+                        repository.Add(country);
+                        repository.Save();
+
+                        country_text.id_drzava = country.id;
+                        country_text.id_jezik = clId;
+                        country_text.opis = "";
+                        UpdateModel(country_text);
+                        repository.Add(country_text);
+                        repository.Save();
+                        //return RedirectToAction("Details", new { id = country_text.id });
+                    }
+                    catch
+                    {
+                        ModelState.AddRuleViolations(country_text.GetRuleViolations());
+                    }
+
+                var cities = from g in db.grads
+                             from gt in db.grad_teksts
+                             where g.id == gt.id_grad
+                             select gt;
+                foreach(var item in cities)
+                {
+                    if (city_text.naziv == item.naziv) cityBool=true;
+                }
+                if (cityBool==false)
+                    try
+                    {
+                        city.id_drzava=country.id;                    
+                        UpdateModel(city);
+                        repository.Add(city);
+                        repository.Save();
+
+                        city_text.id_grad = city.id;
+                        city_text.id_jezik = clId;
+                        city_text.opis = "";
+                        UpdateModel(city_text);
+                        repository.Add(city_text);
+                        repository.Save();
+                    }
+                    catch
+                    {
+                        ModelState.AddRuleViolations(city_text.GetRuleViolations());
+                    }
+
+
+                var universities = from s in db.sveucilistes
+                                   from st in db.sveuciliste_teksts
+                                   where s.id == st.id_sveuciliste
+                                   select st;
+                foreach(var item in universities)
+                {
+                    if (university_text.naziv == item.naziv) cityBool=true;
+                }
+                if (universityBool==false)
+                    try
+                    {
+                        university.id_grad=city.id;
+                        university.adresa_sveucilista="";
+                        university.broj_telefona=null;
+                        university.web="";
+                        UpdateModel(university);
+                        repository.Add(university);
+                        repository.Save();
+
+                        university_text.id_sveuciliste = university.id;
+                        university_text.id_jezik = clId;
+                        university_text.opis = "";
+                        UpdateModel(university_text);
+                        repository.Add(university_text);
+                        repository.Save();
+                    }
+                    catch
+                    {
+                        ModelState.AddRuleViolations(university_text.GetRuleViolations());
+                    }
+
+                var faculties = from f in db.fakultets
+                                from ft in db.fakultet_teksts
+                                where f.id == ft.id_fakultet
+                                select ft;
+                foreach(var item in faculties)
+                {
+                    if (faculty_text.naziv == item.naziv) cityBool=true;
+                }
+                if (facultyBool==false)
+                    try
+                    {
+                        faculty.id_grad = city.id;
+                        faculty.adresa_fakulteta = "";
+                        faculty.broj_telefona = null;
+                        faculty.web = "";
+                        faculty.id_sveuciliste = university.id;
+                        faculty.slika = "";
+                        UpdateModel(faculty);
+                        repository.Add(faculty);
+                        repository.Save();
+
+                        UpdateModel(faculty);
+                        repository.Add(faculty);
+                        repository.Save();
+                        return RedirectToAction("Details", new { id = faculty.id });
+                   }
+                   catch
+                   {
+                        ModelState.AddRuleViolations(faculty_text.GetRuleViolations());
+                   }
+               }
+            return View(facultyEntry);
         }
 
         public ActionResult Edit(int id)
